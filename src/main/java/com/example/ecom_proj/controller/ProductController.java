5999391e54cb92035a3a275865e1467022b92149
Dilshan <dilshanwijerathna426@gmail.com>
2026-03-30 20:13:28 +0530

    Add Product entity, repository, service, and controller with /products endpoint

--- src/main/java/com/example/ecom_proj/controller/ProductController.java
+++ src/main/java/com/example/ecom_proj/controller/ProductController.java
@@ -1,15 +1,30 @@
 package com.example.ecom_proj.controller;
 
 
+import com.example.ecom_proj.model.Product;
+import com.example.ecom_proj.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class ProductController {
 
+    @Autowired
+    ProductService productService;
+
     @RequestMapping("/")
     public String greet(){
         return "Hello World";
+    }
+
+
+    @GetMapping ("/products")
+    public List<Product> getAllProducts(){
+        return productService.getAllProducts();
     }
 }
